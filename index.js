d5f1905fb8ad851e7d2bcb2f21ad8fa44004f923
kdeguzm3 <58832810+kdeguzm3@users.noreply.github.com>
2020-03-06 10:08:03 -0500

    updated

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
+/*
+This page is not valid javascript, it only shows what is linked in the bookmark href in index.html
+*/
 
-let x = {
-  javascript: function() {
-	let urlArray = Location.href.split('://');
-	urlArray = urlArray[1];
-	urlArray = urlArray.split('/');
-	let linkTree = urlArray.splice(2);
-	urlArray = `${urlArray[1]}.github.io/${linkTree.join('/')}`;
-	Location.assign(urlArray);
-  }
-}
-
-document.querySelector("body").textContent = JSON.stringify(x);
+javascript: 
+let vpath = location.pathname.split("/"); 
+vpath = `${vpath[1]}.github.io/${vpath.splice(2)}`; 
+location.replace(`http://${vpath}`);

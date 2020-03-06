0e98259137eccc36fb4a2081db7df98d861cce34
kdeguzm3 <58832810+kdeguzm3@users.noreply.github.com>
2020-03-06 11:26:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+document.querySelector("#copier").addEventListener("click", function(event) {
+  event.preventDefault();
+  let blurb = document.querySelector("#bookmarklet");
+  blurb.select();
+  document.execCommand('copy');
+});
+
+
 /*
 This page is not valid javascript, it only shows what is linked in the bookmark href in index.html
-*/
+
 
 javascript: 
 let vpath = location.pathname.split("/"); 
 vpath = `${vpath[1]}.github.io/${vpath.splice(2).join("/")}`; 
 location.replace(`http://${vpath}`);
+*/
